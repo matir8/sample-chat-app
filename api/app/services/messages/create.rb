c154a2ea4f922d0fda4a4634200f3e31ea500764
matir8 <martin.markov28@gmail.com>
2022-01-17 02:09:58 +0100

    Prepare for deployment on heroku

--- api/app/services/messages/create.rb
+++ api/app/services/messages/create.rb
@@ -1,35 +1,33 @@
 # frozen_string_literal: true
 
 module Services
-  module Messages
-    class Create
-      def initialize(conversation_id:, user_id:, content:)
-        @conversation_id = conversation_id
-        @user_id = user_id
-        @content = content
-      end
+  class Messages::Create
+    def initialize(conversation_id:, user_id:, content:)
+      @conversation_id = conversation_id
+      @user_id = user_id
+      @content = content
+    end
 
-      def call
-        raise 'Invalid conversation' unless conversation.present? && conversation.accessible_for?(user_id: user_id)
-        raise 'Invalid user' unless user_id.present?
-        raise 'Empty message passed' unless content.present?
+    def call
+      raise 'Invalid conversation' unless conversation.present? && conversation.accessible_for?(user_id: user_id)
+      raise 'Invalid user' unless user_id.present?
+      raise 'Empty message passed' unless content.present?
 
-        message = MessageRepository.new.create!(conversation_id: conversation.id, user_id: user.id, content: content)
+      message = MessageRepository.new.create!(conversation_id: conversation.id, user_id: user.id, content: content)
 
-        ActionCable.server.broadcast(conversation.id, MessageSerializer.new(message))
-      end
+      ActionCable.server.broadcast(conversation.id, MessageSerializer.new(message))
+    end
 
-      private
+    private
 
-      attr_reader :conversation_id, :user_id, :content
+    attr_reader :conversation_id, :user_id, :content
 
-      def conversation
-        @conversation ||= ConversationRepository.new.find_by_id(id: conversation_id)
-      end
+    def conversation
+      @conversation ||= ConversationRepository.new.find_by_id(id: conversation_id)
+    end
 
-      def user
-        @user ||= UserRepository.new.find_by_id(id: user_id)
-      end
+    def user
+      @user ||= UserRepository.new.find_by_id(id: user_id)
     end
   end
 end
