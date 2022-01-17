b58139e27340579ff90bf1896d9f17668f032d7e
matir8 <martin.markov28@gmail.com>
2022-01-17 01:44:44 +0100

    Adds user validations

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_and_belongs_to_many :conversations
+
+  validates :email, uniqueness: true
+  validates :nickname, uniqueness: true
 end
